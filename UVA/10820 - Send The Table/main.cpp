328d70631c7734699a7470fe12d9985f5a12e376
kittobi92 <Tobias.Heuer@gmx.net>
2016-05-10 13:54:19 +0200

    solve 10820 and fix bug in factorization algorithm

--- UVA/10820 - Send The Table/main.cpp
+++ UVA/10820 - Send The Table/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 typedef long long ll;
-
+typedef pair<ll,ll> pll;
 #define N 50000
+
 vector<ll> primes;
-typedef pair<int,int> ii;
 
 //Finds all primes number between 0..N
 //Use this method for N < 10000000 to avoid memory access errors
@@ -22,12 +22,13 @@ void primeSieve() {
   }
 }
 
+
 //Factorize a number n in its prime factors
 //Call primeSieve-method before with N > sqrt(n)
 //Return: Returns a vector of pairs, where the first entry in the pair is
 //the prime factor p and the second counts how many times p divides n
-vector<ii> factorize(ll n) {
-  vector<ii> fact; ll num = n, i = 0, c = 0;
+vector<pll> factorize(ll n) {
+  vector<pll> fact; ll num = n, i = 0, c = 0;
   while(num != 1) {
     if(num % primes[i] == 0) {
       c++; num /= primes[i];
@@ -43,14 +44,31 @@ vector<ii> factorize(ll n) {
   return fact;
 }
 
+ll eulerPi(ll n) {
+  vector<pll> fact = factorize(n);
+  ll pi = 1;
+  for(int i = 0; i < fact.size(); i++)
+    pi *= pow(fact[i].first,fact[i].second-1)*(fact[i].first - 1);
+  return pi;
+}
+
 int main() {
+  
   primeSieve();
-  ll n = 10;
-  vector<ii> fact = factorize(n);
-  ll num = 1;
-  for(int i = 0; i < fact.size(); i++) {
-    num *= pow(fact[i].first,fact[i].second);
-    cout << fact[i].first << "^" << fact[i].second << (i == fact.size()-1 ? " = " : " * ");
+  vector<ll> eulerPrecalc(N+1,0);
+  eulerPrecalc[1] = 1;
+  for(ll i = 2; i <= N; ++i)
+    eulerPrecalc[i] = eulerPrecalc[i-1] + 2*eulerPi(i);
+  
+  
+  ll n;
+  while(cin >> n) {
+    if(n == 0)
+      break;
+    
+    cout << eulerPrecalc[n] << endl;
+    
   }
-  cout << num << (n == num ? " = " : " != ") << n << endl;
+  
+  return 0;
 }
