f2f4b07a2df52848f0c8f75879ad49263a2be56b
kittobi1992 <tobias.heuer@gmx.net>
2016-11-04 18:01:01 +0100

    solve D neerc 2015

--- Codeforces/NEERC 2015/D/main.cpp
+++ Codeforces/NEERC 2015/D/main.cpp
@@ -2,10 +2,41 @@
 
 using namespace std;
 
-ifstream fin("f.in");
-ofstream fout("f.out");
+using ll = unsigned long long;
+
+ll N; 
+ll res[100];
+
+int solve(ll n) {
+    if(n == 0) return 0;  
+    
+    if(n % 2 == 0) {
+        int len = solve(n/2);
+        for(int i = 0; i < len; ++i) res[i] *= 2;
+        return len;
+    }
+    
+    ll three = 1;
+    while(n / 3 >= three)  three *= 3;
+    int len = solve(n-three);
+    res[len++] = three;
+    return len;
+    
+}
 
 int main() {
   
+    freopen("distribution.in", "r",stdin);
+    freopen("distribution.out", "w",stdout);
+    
+    int T; cin >> T;
+    while(T--) {
+        cin >> N;
+        for(int i = 0; i < 100; ++i) res[i] = 0;
+        int len = solve(N);
+        cout << len << endl;
+        for(int i = 0; i < len; ++i) cout << res[i] << (i == len-1 ? "\n" : " ");
+    }
+    
 	return 0;
 }
